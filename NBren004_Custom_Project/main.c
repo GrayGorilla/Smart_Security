99c9d66ff96d7eaf6ba863e83afeb795db83caeb
Nathan Brennan <nbren004@ucr.edu>
2019-05-23 20:18:33 -0700

    Started Atmel AVR Project

---
+++ NBren004_Custom_Project/main.c
@@ -0,0 +1,25 @@
+/*
+ * NBren004_Custom_Project.c
+ *
+ * Created: 5/23/2019 1:51:25 AM
+ * Author : Nathan Brennan
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "nokia5110.h"
+
+
+int main(void)
+{
+    nokia_lcd_init();
+    nokia_lcd_clear();
+    nokia_lcd_write_string("IT'S WORKING!",1);
+    nokia_lcd_set_cursor(0, 15);
+    nokia_lcd_write_string("Nice!", 3);
+    nokia_lcd_render();
+
+    while (1) {
+        _delay_ms(1000);
+    }
+}
