2605c5a9a1fb5faade5ee235ccac108f505bf4cb
Nathan Brennan <nbren004@ucr.edu>
2019-05-31 01:54:18 -0700

    Recieve input from keypad, display on Nokia

---
+++ NBren004_Custom_Project/keypad.h
@@ -0,0 +1,55 @@
+// Returns '\0' if no key pressed, else returns char '1', '2', ... '9', 'A', ...
+// If multiple keys pressed, returns leftmost-topmost one
+// Keypad must be connected to port A
+/* Keypad arrangement
+        PA4 PA5 PA6 PA7
+   col  1   2   3   4
+row
+PC0 1   1 | 2 | 3 | A
+PC1 2   4 | 5 | 6 | B
+PC2 3   7 | 8 | 9 | C
+PC3 4   * | 0 | # | D
+*/
+
+#define THE_PORT PORTC
+#define THE_PIN PINC
+// Must initialize DDR in main.c
+
+unsigned char GetKeypadKey() {
+
+	THE_PORT = 0xEF; // Enable col 4 with 0, disable others with 1’s
+	asm("nop"); // add a delay to allow THE_PORT to stabilize before checking
+	if (GetBit(THE_PIN,0)==0) { return('1'); }
+	if (GetBit(THE_PIN,1)==0) { return('4'); }
+	if (GetBit(THE_PIN,2)==0) { return('7'); }
+	if (GetBit(THE_PIN,3)==0) { return('*'); }
+
+	// Check keys in col 2
+	THE_PORT = 0xDF; // Enable col 5 with 0, disable others with 1’s
+	asm("nop"); // add a delay to allow THE_PORT to stabilize before checking
+	if (GetBit(THE_PIN,0)==0) { return('2'); }
+	if (GetBit(THE_PIN,1)==0) { return('5'); }
+	if (GetBit(THE_PIN,2)==0) { return('8'); }
+	if (GetBit(THE_PIN,3)==0) { return('0'); }
+
+
+	// Check keys in col 3
+	THE_PORT = 0xBF; // Enable col 6 with 0, disable others with 1’s
+	asm("nop"); // add a delay to allow THE_PORT to stabilize before checking
+	if (GetBit(THE_PIN,0)==0) { return('3'); }
+	if (GetBit(THE_PIN,1)==0) { return('6'); }
+	if (GetBit(THE_PIN,2)==0) { return('9'); }
+	if (GetBit(THE_PIN,3)==0) { return('#'); }
+
+
+	// Check keys in col 4	
+    THE_PORT = 0x7F;
+    asm("nop");
+	if (GetBit(THE_PIN,0)==0) { return('A'); }
+	if (GetBit(THE_PIN,1)==0) { return('B'); }
+	if (GetBit(THE_PIN,2)==0) { return('C'); }
+	if (GetBit(THE_PIN,3)==0) { return('D'); }
+
+
+	return('\0'); // default value
+}
--- NBren004_Custom_Project/main.c
+++ NBren004_Custom_Project/main.c
@@ -9,14 +9,154 @@
 #include <util/delay.h>
 #include "nokia5110.h"
 
+#define HIDE 0
+#define SHOW 1
 
-int main(void)
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <string.h>
+#define TASKS_SIZE 2
+#define TASKS_PERIOD 100
+#include "bit.h"
+#include "scheduler.h"
+#include "timer.h"
+#include "keypad.h"
+
+char* message;
+unsigned char position;
+
+
+/* ----------------------- STATE MACHINES ----------------------- */
+
+enum lockStates { Start, Reset, Locked, Unlocked, Armed, SoundAlarm };
+int place_holder(){};
+
+
+enum keyPadStates{KP_Start, Button};
+int keyPad_tick(int state) {
+    static char input;
+    unsigned char mutate;
+    // Transitions
+    switch (state) {
+        case KP_Start :
+            state = Button;
+            message = "Type over me!                   ";
+            position = 0;
+            mutate = 0;
+            break;
+        case Button :
+            state = Button;
+            break;
+        default:
+            break;
+    }
+    // Actions
+    switch (state) {
+        case Button :
+            input = GetKeypadKey();
+            if (input) mutate = 1;
+            else mutate = 0;
+        
+            if (mutate) {
+                message[position] = input;
+                if (position >= 31) position = 0;
+                else position++;
+            }
+            break;
+        default:
+            break;
+    }
+    return state;
+}
+
+enum LCD_States { LCD_Start, Display };
+int LCD_tick(int state) {
+    // Transitions
+    switch (state) {
+        case LCD_Start:
+            state = Display;
+            break;
+        case Display:
+            state = Display;
+            break;
+        default:
+            state = LCD_Start;
+            break;
+    }
+    // Actions
+    switch (state) {
+        case Display:
+        
+        /*
+            nokia_lcd_write_string("IT'S WORKING!", 1);
+            nokia_lcd_set_cursor(0, 15);
+        */
+            nokia_lcd_clear();
+            nokia_lcd_write_string(message, 1);
+            nokia_lcd_set_cursor(0, position + 1);
+            nokia_lcd_render();
+
+            //LCD_DisplayString(1, message);
+            //LCD_Cursor(position + 1);
+
+            break;
+        default:
+            break;
+    }
+    return state;
+}
+
+
+/* ----------------------- MAIN ----------------------- */
+
+int main(void) {
+    
+    DDRC = 0xF0; PORTC = 0x0F;  // Keypad input
+    
+    unsigned char i = 0;
+    tasks[i].state = KP_Start;
+    tasks[i].period = TASKS_PERIOD;
+    tasks[i].elapsedTime = tasks[i].period;
+    tasks[i].TickFct = &keyPad_tick;
+    i++;
+    tasks[i].state = LCD_Start;
+    tasks[i].period = TASKS_PERIOD;
+    tasks[i].elapsedTime = tasks[i].period;
+    tasks[i].TickFct = &LCD_tick;
+    i++;
+    
+    TimerSet(TASKS_PERIOD);
+    TimerOn();
+    // Initializes the LCD display
+//    LCD_init();
+
+    nokia_lcd_init();
+    nokia_lcd_clear();
+    
+    
+    while (1) {}
+}
+
+
+
+
+int main1(void)
 {
     nokia_lcd_init();
     nokia_lcd_clear();
-    nokia_lcd_write_string("IT'S WORKING!",1);
+    nokia_lcd_write_string("IT'S WORKING!", 1);
     nokia_lcd_set_cursor(0, 15);
     nokia_lcd_write_string("Nice!", 3);
+    /*
+    nokia_lcd_set_pixel(5, 0, SHOW);
+    nokia_lcd_set_pixel(5, 1, SHOW);
+    nokia_lcd_set_pixel(5, 2, SHOW);
+    nokia_lcd_set_pixel(5, 3, SHOW);
+    nokia_lcd_set_pixel(5, 0, HIDE);
+    */
     nokia_lcd_render();
 
     while (1) {
